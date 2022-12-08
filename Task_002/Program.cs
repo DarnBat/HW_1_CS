03a88665a5c2d721a158b326ca1cbafdb52a6249
Vladislav.M <lidpid@gmail.com>
2022-12-09 01:44:11 +0600

    Сделал задание 2

---
+++ Task_002/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число 1 ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2 ");
+int b = Convert.ToInt32(Console.ReadLine());
+if ( a > b)
+{
+Console.Write("Max = " + a);
+}
+else{
+    Console.Write("Max = " + b);
+}
