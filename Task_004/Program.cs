04954e50026628db59f40a158e0020d00473f119
Vladislav.M <lidpid@gmail.com>
2022-12-09 02:00:27 +0600

    Сделал задание 4

---
+++ Task_004/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число 1");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2 ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 3 ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if( a > b && a > c){
+    Console.Write("Max = " + a);
+}
+else if(b > c) {
+    Console.Write("Max = " + b);
+}
+else{
+    Console.Write("Max = " + c);
+}
